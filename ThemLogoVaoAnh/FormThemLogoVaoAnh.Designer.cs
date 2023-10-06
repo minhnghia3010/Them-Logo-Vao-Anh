77b1cb5d808eeb9172ae498d459265d5078ad166
minhnghia3010 <vominhnghia3010@gmail.com>
2023-10-06 16:16:49 +0700

    ThemLogoVaoAnh

---
+++ ThemLogoVaoAnh/FormThemLogoVaoAnh.Designer.cs
@@ -0,0 +1,261 @@
+﻿
+namespace ThemLogoVaoAnh
+{
+    partial class FormThemLogoVaoAnh
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnthumuc = new System.Windows.Forms.Button();
+            this.txtchu = new System.Windows.Forms.TextBox();
+            this.cbmau = new System.Windows.Forms.ComboBox();
+            this.chkauto = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtsize = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtX = new System.Windows.Forms.TextBox();
+            this.txtY = new System.Windows.Forms.TextBox();
+            this.cbxX = new System.Windows.Forms.ComboBox();
+            this.cbxY = new System.Windows.Forms.ComboBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // btnthumuc
+            // 
+            this.btnthumuc.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnthumuc.Location = new System.Drawing.Point(15, 396);
+            this.btnthumuc.Name = "btnthumuc";
+            this.btnthumuc.Size = new System.Drawing.Size(773, 70);
+            this.btnthumuc.TabIndex = 0;
+            this.btnthumuc.Text = "Chọn Thư Mục Cần Thêm Logo";
+            this.btnthumuc.UseVisualStyleBackColor = true;
+            this.btnthumuc.Click += new System.EventHandler(this.btnthumuc_Click);
+            // 
+            // txtchu
+            // 
+            this.txtchu.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtchu.Location = new System.Drawing.Point(178, 89);
+            this.txtchu.Name = "txtchu";
+            this.txtchu.Size = new System.Drawing.Size(388, 30);
+            this.txtchu.TabIndex = 1;
+            // 
+            // cbmau
+            // 
+            this.cbmau.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbmau.FormattingEnabled = true;
+            this.cbmau.Location = new System.Drawing.Point(178, 151);
+            this.cbmau.Name = "cbmau";
+            this.cbmau.Size = new System.Drawing.Size(269, 33);
+            this.cbmau.TabIndex = 2;
+            // 
+            // chkauto
+            // 
+            this.chkauto.AutoSize = true;
+            this.chkauto.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkauto.Location = new System.Drawing.Point(432, 288);
+            this.chkauto.Name = "chkauto";
+            this.chkauto.Size = new System.Drawing.Size(102, 26);
+            this.chkauto.TabIndex = 3;
+            this.chkauto.Text = "Tự Động";
+            this.chkauto.UseVisualStyleBackColor = true;
+            this.chkauto.CheckedChanged += new System.EventHandler(this.chkauto_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(237, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(329, 32);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "THÊM LOGO VÀO ẢNH";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(139, 23);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Nhập Tên Logo";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 156);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(145, 23);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Nhập Màu Logo";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(14, 222);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(143, 23);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Nhập Size Logo";
+            // 
+            // txtsize
+            // 
+            this.txtsize.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtsize.Location = new System.Drawing.Point(178, 217);
+            this.txtsize.Name = "txtsize";
+            this.txtsize.Size = new System.Drawing.Size(186, 30);
+            this.txtsize.TabIndex = 8;
+            this.txtsize.Leave += new System.EventHandler(this.txtsize_Leave);
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.HelpRequest += new System.EventHandler(this.folderBrowserDialog1_HelpRequest);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(14, 285);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 23);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Trục X";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(14, 348);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(66, 23);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Trục Y";
+            // 
+            // txtX
+            // 
+            this.txtX.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtX.Location = new System.Drawing.Point(178, 282);
+            this.txtX.Name = "txtX";
+            this.txtX.Size = new System.Drawing.Size(248, 32);
+            this.txtX.TabIndex = 13;
+            // 
+            // txtY
+            // 
+            this.txtY.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtY.Location = new System.Drawing.Point(178, 341);
+            this.txtY.Name = "txtY";
+            this.txtY.Size = new System.Drawing.Size(248, 32);
+            this.txtY.TabIndex = 14;
+            // 
+            // cbxX
+            // 
+            this.cbxX.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbxX.FormattingEnabled = true;
+            this.cbxX.Location = new System.Drawing.Point(540, 283);
+            this.cbxX.Name = "cbxX";
+            this.cbxX.Size = new System.Drawing.Size(248, 33);
+            this.cbxX.TabIndex = 15;
+            // 
+            // cbxY
+            // 
+            this.cbxY.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbxY.FormattingEnabled = true;
+            this.cbxY.Location = new System.Drawing.Point(540, 340);
+            this.cbxY.Name = "cbxY";
+            this.cbxY.Size = new System.Drawing.Size(248, 33);
+            this.cbxY.TabIndex = 16;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.Location = new System.Drawing.Point(592, 91);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(74, 26);
+            this.checkBox1.TabIndex = 17;
+            this.checkBox1.Text = "Xoay";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // FormThemLogoVaoAnh
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(802, 486);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.cbxY);
+            this.Controls.Add(this.cbxX);
+            this.Controls.Add(this.txtY);
+            this.Controls.Add(this.txtX);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtsize);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chkauto);
+            this.Controls.Add(this.cbmau);
+            this.Controls.Add(this.txtchu);
+            this.Controls.Add(this.btnthumuc);
+            this.Name = "FormThemLogoVaoAnh";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Thêm Logo Vào Ảnh";
+            this.Load += new System.EventHandler(this.FormThemLogoVaoAnh_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnthumuc;
+        private System.Windows.Forms.TextBox txtchu;
+        private System.Windows.Forms.ComboBox cbmau;
+        private System.Windows.Forms.CheckBox chkauto;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtsize;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtX;
+        private System.Windows.Forms.TextBox txtY;
+        private System.Windows.Forms.ComboBox cbxX;
+        private System.Windows.Forms.ComboBox cbxY;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ ThemLogoVaoAnh/FormThemLogoVaoAnh.cs
@@ -0,0 +1,359 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+
+namespace ThemLogoVaoAnh
+{
+    public partial class FormThemLogoVaoAnh : Form
+    {
+        public FormThemLogoVaoAnh()
+        {
+            InitializeComponent();
+            this.folderBrowserDialog1.Description =
+            "Chọn thư mục chứa ảnh";
+            this.folderBrowserDialog1.ShowNewFolderButton = false;
+        }
+
+        private void FormThemLogoVaoAnh_Load(object sender, EventArgs e)
+        {
+
+            cbxX.Items.Add("Giữa");
+            cbxY.Items.Add("Giữa");
+            cbxX.Items.Add("Trên");
+            cbxY.Items.Add("Trên");
+            cbxX.Items.Add("Dưới");
+            cbxY.Items.Add("Dưới");
+            cbxX.Items.Add("Trái");
+            cbxY.Items.Add("Trái");
+            cbxX.Items.Add("Phải");
+            cbxY.Items.Add("Phải");
+            cbxX.Items.Add("Góc Phải Dưới");
+            cbxY.Items.Add("Góc Phải Dưới");
+            cbxX.Items.Add("Góc Trái Dưới");
+            cbxY.Items.Add("Góc Trái Dưới");
+            cbxX.Items.Add("Góc Phải Trên");
+            cbxY.Items.Add("Góc Phải Trên");
+            cbxX.Items.Add("Góc Trái Trên");
+            cbxY.Items.Add("Góc Trái Trên");
+            cbmau.Items.Add("Red");
+            cbmau.Items.Add("Yellow");
+            cbmau.Items.Add("White");
+            cbmau.Items.Add("Black");
+            cbmau.Items.Add("Blue");
+            cbmau.Items.Add("Orange");
+            cbmau.Items.Add("Green");
+            cbmau.Items.Add("Silver");
+            cbmau.Items.Add("WhiteSmoke");
+            cbmau.Items.Add("Gold");
+            cbmau.Items.Add("Gray");
+            cbmau.Items.Add("LightGreen");
+            cbmau.Items.Add("LightYellow");
+            cbmau.SelectedIndex = 0;
+            chkauto.Checked = false;
+            cbxX.Enabled = false;
+            cbxY.Enabled = false;
+            try
+            {
+                StreamReader sr = new StreamReader(Application.StartupPath + "/luu.txt");
+                string Line = sr.ReadLine();
+                while ((Line != null) && (Line != " "))
+                {
+                    string[] mang = Line.Split(';');
+                    txtchu.Text = mang[0];
+                    cbmau.SelectedIndex = int.Parse(mang[1]);
+                    txtsize.Text = mang[2];
+                    Line = sr.ReadLine();
+                }
+                sr.Close();
+            }
+            catch { }
+
+        }
+        private void btnthumuc_Click(object sender, EventArgs e)
+        {
+            if (cbxX.Text != cbxY.Text)
+            {
+                MessageBox.Show("Vui lòng chọn thông số của X và Y giống nhau");
+            }
+            else
+            {
+                string path = String.Empty;
+                DialogResult result = folderBrowserDialog1.ShowDialog();
+                if (result == DialogResult.OK)
+                {
+                    path = folderBrowserDialog1.SelectedPath;
+                }
+
+                if (path == String.Empty)
+                {
+
+                    return;
+                }
+                Image img = null;
+                string fullPath = String.Empty;
+
+                try
+                {
+                    string[] imgExtension = { "*.png", "*.jpg", "*.jpeg", ".gif", "*.bmp" };
+                    List<FileInfo> files = new List<FileInfo>();
+
+                    DirectoryInfo dir = new DirectoryInfo(path);
+
+                    foreach (string ext in imgExtension)
+                    {
+                        FileInfo[] folder = dir.GetFiles(ext, SearchOption.AllDirectories);
+                        foreach (FileInfo file in folder)
+                        {
+                            FileStream fs = file.OpenRead();
+                            fullPath = path + @"\" + file.Name;
+
+                            Stream outputStream = new MemoryStream();
+                            AddWatermark(fs, txtchu, outputStream);
+                            fs.Close();
+                            file.Delete();
+                            img = Image.FromStream(outputStream);
+
+                            using (Bitmap savingImage = new Bitmap(img.Width, img.Height, img.PixelFormat))
+                            {
+                                using (Graphics g = Graphics.FromImage(savingImage))
+                                    g.DrawImage(img, new Point(0, 0));
+                                savingImage.Save(fullPath, ImageFormat.Jpeg);
+                            }
+                            img.Dispose();
+                        }
+                    }
+                    MessageBox.Show("Thêm Logo vào hình xong");
+                    try
+                    {
+                        StreamWriter sw = new StreamWriter(Application.StartupPath + "/luu.txt");
+                        string Line = "";
+                        Line = txtchu.Text + ";" + cbmau.SelectedIndex + ";" + txtsize.Text + ";";
+                        sw.WriteLine(Line);
+                        sw.Close();
+                    }
+                    catch
+                    { }
+                }
+                catch
+                {
+                    MessageBox.Show("Lỗi rồi kiểm tra lại đi, VD: tên màu đúng chưa?");
+                }
+                finally
+                {
+                    if (img != null)
+                        img.Dispose();
+                }
+            }
+        }
+        public void AddWatermark(FileStream fs, TextBox watermarkText, Stream outputStream)
+        {
+            Image img = Image.FromStream(fs);
+            int width;
+            int height;
+            if (!chkauto.Checked)
+            {
+                width = Int16.Parse(txtX.Text);
+                height = Int16.Parse(txtY.Text);
+            }
+            else
+            {
+                if (!checkBox1.Checked)
+                {
+                    if (cbxX.Text == "Giữa" && cbxY.Text == "Giữa")
+                    {
+                        width = (img.Width / 2) - 235;
+                        height = (img.Height / 2) - 100;
+                    }
+                    else
+                    {
+                        width = 500;
+                        height = 500;
+                    }
+                    if (cbxX.Text == "Trên" && cbxY.Text == "Trên")
+                    {
+                        width = (img.Width / 2) - 235;
+                        height = (img.Height / 2) - 1070;
+                    }
+                    if (cbxX.Text == "Dưới" && cbxY.Text == "Dưới")
+                    {
+                        width = (img.Width / 2) - 235;
+                        height = (img.Height / 2) + 910;
+                    }
+                    if (cbxX.Text == "Góc Phải Dưới" && cbxY.Text == "Góc Phải Dưới")
+                    {
+                        width = (img.Width / 2) + 1500;
+                        height = (img.Height / 2) + 910;
+                    }
+                    if (cbxX.Text == "Góc Trái Dưới" && cbxY.Text == "Góc Trái Dưới")
+                    {
+                        width = (img.Width / 2) - 1900;
+                        height = (img.Height / 2) + 910;
+                    }
+                    if (cbxX.Text == "Trái" && cbxY.Text == "Trái")
+                    {
+                        width = (img.Width / 2) - 1900;
+                        height = (img.Height / 2) - 100;
+                    }
+                    if (cbxX.Text == "Phải" && cbxY.Text == "Phải")
+                    {
+                        width = (img.Width / 2) + 1500;
+                        height = (img.Height / 2) - 100;
+                    }
+                    if (cbxX.Text == "Góc Trái Trên" && cbxY.Text == "Góc Trái Trên")
+                    {
+                        width = (img.Width / 2) - 1900;
+                        height = (img.Height / 2) - 1070;
+                    }
+                    if (cbxX.Text == "Góc Phải Trên" && cbxY.Text == "Góc Phải Trên")
+                    {
+                        width = (img.Width / 2) + 1500;
+                        height = (img.Height / 2) - 1070;
+                    }
+                }
+                else
+                {
+                    if (cbxX.Text == "Giữa" && cbxY.Text == "Giữa")
+                    {
+                        width = (img.Width / 2) - 40;
+                        height = (img.Height / 2) - 1750;
+                    }
+                    else
+                    {
+                        width = 500;
+                        height = 500;
+                    }
+                    if (cbxX.Text == "Trên" && cbxY.Text == "Trên")
+                    {
+                        width = (img.Width / 2) - 600;
+                        height = (img.Height / 2) - 2350;
+                    }
+                    if (cbxX.Text == "Dưới" && cbxY.Text == "Dưới")
+                    {
+                        width = (img.Width / 2) - (-550);
+                        height = (img.Height / 2) - 1150;
+                    }
+                    if (cbxX.Text == "Góc Phải Dưới" && cbxY.Text == "Góc Phải Dưới")
+                    {
+                        width = (img.Width / 2) -(-1850);
+                        height = (img.Height / 2) - 2350;
+                    }
+                    if (cbxX.Text == "Góc Trái Dưới" && cbxY.Text == "Góc Trái Dưới")
+                    {
+                        width = (img.Width / 2) - 600;
+                        height = (img.Height / 2) - 10;
+                    }
+                    if (cbxX.Text == "Trái" && cbxY.Text == "Trái")
+                    {
+                        width = (img.Width / 2) + (-1200);
+                        height = (img.Height / 2) - 500;
+                    }
+                    if (cbxX.Text == "Phải" && cbxY.Text == "Phải")
+                    {
+                        width = (img.Width / 2) - (-1100);
+                        height = (img.Height / 2) - 3000;
+                    }
+                    if (cbxX.Text == "Góc Trái Trên" && cbxY.Text == "Góc Trái Trên")
+                    {
+                        width = (img.Width / 2) +(-1850);
+                        height = (img.Height / 2) - 1150;
+                    }
+                    if (cbxX.Text == "Góc Phải Trên" && cbxY.Text == "Góc Phải Trên")
+                    {
+                        width = (img.Width / 2) - (-600);
+                        height = (img.Height / 2) - 3500;
+                    }
+                }
+            }
+
+            int size;
+            Font font;
+            if (chkauto.Checked) size = img.Width * 10 / watermarkText.Width;
+            else
+                size = int.Parse(txtsize.Text);
+            font = new Font("Tamoha", size, FontStyle.Bold, GraphicsUnit.Pixel);
+            Color color = Color.FromName(cbmau.SelectedItem.ToString());
+            Point pt = new Point(width, height);
+            SolidBrush sbrush = new SolidBrush(color);
+            Graphics gr = null;
+            try
+            {
+                gr = Graphics.FromImage(img);
+            }
+            catch
+            {
+                Image img1 = img;
+                img = new Bitmap(img.Width, img.Height);
+                gr = Graphics.FromImage(img);
+                gr.DrawImage(img1, new Rectangle(0, 0, img.Width, img.Height), 0, 0, img.Width, img.Height, GraphicsUnit.Pixel);
+                img1.Dispose();
+            }
+            if (checkBox1.Checked)
+            {
+                gr.MeasureString(watermarkText.Text.ToString(), font);
+                gr.RotateTransform(45);
+            }
+            gr.DrawString(watermarkText.Text.ToString(), font, sbrush, pt);
+            gr.Dispose();
+            img.Save(outputStream, ImageFormat.Jpeg);
+        }
+
+        private void txtsize_Leave(object sender, EventArgs e)
+        {
+            try
+            {
+                int.Parse(txtsize.Text);
+                if (int.Parse(txtsize.Text) <= 0)
+                {
+                    MessageBox.Show("nhập size không đúng, xem lại đi..");
+                    txtsize.Focus();
+                }
+            }
+            catch
+            {
+                MessageBox.Show("nhập size không đúng, xem lại đi..");
+                txtsize.Focus();
+            }
+        }
+
+        private void chkauto_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkauto.Checked)
+            {
+                txtsize.Clear();
+                txtsize.Enabled = false;
+                txtX.Enabled = false;
+                txtY.Enabled = false;
+                cbxX.Enabled = true;
+                cbxY.Enabled = true;
+            }
+            else
+            {
+                txtsize.Enabled = true;
+                txtX.Enabled = true;
+                txtY.Enabled = true;
+                cbxX.Enabled = false;
+                cbxY.Enabled = false;
+            }
+        }
+
+        private void checkBoxLamMo_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+
